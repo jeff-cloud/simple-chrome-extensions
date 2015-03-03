44e5c64efb892c154171a88d2c12b1202643549d
U-CORP\jhuang <jeff.h.max@gmail.com>
2015-03-03 18:38:06 -0500

    Version 1.1: implemented Bring All buttons

--- FindTabs/popup.js
+++ FindTabs/popup.js
@@ -2,19 +2,60 @@
 	var app = angular.module("app", []);
 
 	var ctrl = app.controller("tabCtrl", function($scope) {
-		chrome.windows.getAll({ populate : true }, function(windows) {
-			$scope.$apply(function() {
-				$scope.result = { windows: windows};
+		var getAllTabs = function() {
+			chrome.windows.getAll(
+				{ populate : true }, 
+				function(windows) {
+			
+			chrome.windows.getCurrent({}, function(currWindow) {
+				$scope.$apply(function() {
+					$scope.otherWindows = [];
+					$scope.allWindows = windows;
+					$scope.currWindow = currWindow;
+
+					$scope.allWindows.forEach(function(w) {
+						if($scope.currWindow.id !== w.id) {
+							$scope.otherWindows.push(w);
+						}
+					});
+
+					$scope.allTabs = [];
+
+					windows.forEach(function( w ) {
+						w.tabs.forEach(function(t) {
+							$scope.allTabs.push(t);
+						});
+					});
+				});
 			});
-		});
+
+				}); //windows.getAll
+		};
+
+		getAllTabs();
 
 		$scope.moveTab = function(w, t) {
-			console.log("Moving " + w + " : " + t);
 			chrome.windows.getCurrent({}, function(cw) {
 				chrome.tabs.move(t, {windowId : cw.id, index : -1 },  function(tab) {
-					console.log(tab.title);
+					//console.log(tab.title);
+					getAllTabs();
 				});
 			});
 		};
+
+		$scope.moveAllTabs = function(tabs) {
+			var tabIds = [];
+			tabs.forEach(function(tab) {
+				tabIds.push(tab.id);
+			});
+
+			chrome.windows.getCurrent({}, function(cw) {
+				chrome.tabs.move(tabIds, {windowId : cw.id, index : -1 },  function(tabs) {
+					//console.log(tab.title);
+					getAllTabs();
+				});
+			});
+
+		}
 	});
 } () );
